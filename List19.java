63382257fa3358998665ccec75b2420182982f72
nicolasbborges-glitch <nicolas.b.borges@aluno.senai.br>
2025-10-03 16:45:09 -0300

    Add files via upload

--- List19.java
+++ List19.java
@@ -1,75 +1,127 @@
-package firstProject;
+package primeiroProjeto;
 
-public class List18 {
-	public static void main(String[] args) {
+import java.util.Scanner;
 
-		Apresentacao();
-		Frase();
-		Contagem();
-		Vogais();
-		Quadrado();
-		Pares();
-		Regressiva();
-		Triangulo();
-		DiasSemana();
-	}
-	
-	public static void Apresentacao() {
-		System.out.println("Nicolas Barros Borges");
-	}
+public class Lista019 {
 
-	public static void Frase() {
-		System.out.println("Só é possível fazer o impossível se acreditar que é possível");
-	}
+		// Função separadora (extra)
+	    public static void separador() {
+	        System.out.println("--------------------");
+	    }
+			
+			// 1 – Classificando valores
+		    public static void classificarValor(int numero) {
+		        if (numero > 0) {
+		            System.out.println("Positivo");
+		        } else if (numero < 0) {
+		            System.out.println("Negativo");
+		        } else {
+		            System.out.println("Zero");
+		        }
+		    }
 
-	public static void Contagem() {
-		int i = 1;
-		do {
-			System.out.println(i);
-			i++;
-		} while (i <= 10);
-	}
+		    // 2 – Maioridade
+		    public static void verificarMaioridade(int idade) {
+		        if (idade >= 18) {
+		            System.out.println("Maior de idade");
+		        } else {
+		            System.out.println("Menor de idade");
+		        }
+		    }
 
-	public static void Vogais() {
-		System.out.println("A E I O U");
-	}
+		    // 3 – Boas Vindas
+		    public static void boasVindas(String nome) {
+		        System.out.println("Bem-vindo(a), " + nome + "!");
+		    }
 
-	public static void Quadrado() {
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				System.out.print("* ");
-			}
-			System.out.println();
+		    // 4 – Tabuada
+		    public static void tabuada(int numero) {
+		        System.out.println("Tabuada de " + numero + ":");
+		        for (int i = 1; i <= 10; i++) {
+		            System.out.println(numero + " x " + i + " = " + (numero * i));
+		        }
+		    }
+
+		    // 5 – Juntando palavras
+		    public static void juntarPalavras(String p1, String p2) {
+		        System.out.println("Concatenadas: " + p1 + " " + p2);
+		    }
+
+		    // 6 – Verificando par ou ímpar
+		    public static void parOuImpar(int numero) {
+		        if (numero % 2 == 0) {
+		            System.out.println(numero + " é Par");
+		        } else {
+		            System.out.println(numero + " é Ímpar");
+		        }
+		    }
+
+		    // 7 – Classificando temperatura
+		    public static void classificarTemperatura(int temp) {
+		        if (temp < 15) {
+		            System.out.println("Frio");
+		        } else if (temp <= 25) {
+		            System.out.println("Agradável");
+		        } else {
+		            System.out.println("Quente");
+		        }
+		    }
+
+		    // 8 – Saudação por turno
+		    public static void saudacaoTurno(String turno) {
+		        switch (turno.toLowerCase()) {
+		            case "manhã":
+		                System.out.println("Bom dia!");
+		                break;
+		            case "tarde":
+		                System.out.println("Boa tarde!");
+		                break;
+		            case "noite":
+		                System.out.println("Boa noite!");
+		                break;
+		            default:
+		                System.out.println("Turno inválido!");
+		        }
+		    }
+
+		    // 9 – Calculando média
+		    public static void calcularMedia(double n1, double n2, double n3) {
+		        double media = (n1 + n2 + n3) / 3;
+		        System.out.println("Média = " + media);
+		    }
+
+		    // 10 – Verificando aprovação
+		    public static void verificarAprovacao(double nota) {
+		        if (nota >= 7) {
+		            System.out.println("Aprovado");
+		        } else if (nota >= 5) {
+		            System.out.println("Em recuperação");
+		        } else {
+		            System.out.println("Reprovado");
+		        }
+		    }
+
+		    public static void main(String[] args) {
+		        classificarValor(5);
+		        separador();
+		        verificarMaioridade(16);
+		        separador();
+		        boasVindas("Maria");
+		        separador();
+		        tabuada(7);
+		        separador();
+		        juntarPalavras("Olá", "Mundo");
+		        separador();
+		        parOuImpar(10);
+		        separador();
+		        classificarTemperatura(22);
+		        separador();
+		        saudacaoTurno("tarde");
+		        separador();
+		        calcularMedia(8.5, 7.0, 6.5);
+		        separador();
+		        verificarAprovacao(6.0);
+		   }
+
+
 		}
-	}
-
-	public static void Pares() {
-		for (int i = 2; i <= 20; i += 2) {
-			System.out.println(i);
-		}
-	}
-
-	public static void Regressiva() {
-		for (int i = 10; i >= 1; i--) {
-			System.out.println(i);
-		}
-		System.out.println("Fim!");
-	}
-
-	public static void Triangulo() {
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
-	}
-
-	public static void DiasSemana() {
-		String[] dias = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
-						"Quinta-feira", "Sexta-feira", "Sábado"};
-		for (String dia : dias) {
-			System.out.println(dia);
-		}
-	}
-}
